e052a9d02f86a71ffe9e8b61f6c541bae84ae4c8
markhilkert <mark.hilkert@gmail.com>
2019-04-03 10:14:37 -0500

    started first post and moved right sidebar to app.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,7 +32,7 @@
                             <article class="post">
 
                                 <div class="post-header">
-                                    <h2 class="post-title"><a href="#">Sleep&#8212It's frustratingly Important. </a></h2>
+                                    <h2 class="post-title"><router-link to="/posts/1">Sleep&#8212It's frustratingly Important. </router-link></h2>
                                     <ul class="post-meta">
                                         <li><i class="mdi mdi-calendar"></i> April 02, 2019</li>
                                         <li><i class="mdi mdi-tag-text-outline"></i> <a href="#">Productivity</a>, <a href="#">Sleep</a></li>
@@ -41,7 +41,7 @@
                                 </div>
 
                                 <div class="post-preview">
-                                    <a href="#"><img src="images/blog/blog-1.jpg" alt="" class="img-fluid rounded"></a>
+                                    <router-link to="/posts/1"><img src="images/blog/blog-1.jpg" alt="a cup of coffee and a book rest on a bed in the sunlight" class="img-fluid rounded"></router-link>
                                 </div>
 
                                 <div class="post-content">
@@ -171,113 +171,7 @@
                         </div>
                         <!-- Content end-->
 
-                        <!-- Sidebar-->
-                        <div class="col-xl-4">
-                            <div class="sidebar">
 
-                                <!-- Search widget-->
-                                <aside class="widget widget_search">
-                                    <form>
-                                        <input class="form-control pr-5" type="search" placeholder="Search...">
-                                        <button class="search-button" type="submit"><span class="mdi mdi-magnify"></span></button>
-                                    </form>
-                                </aside>
-
-                                <aside class="widget about-widget">
-                                    <div class="widget-title">About Me</div>
-
-                                    <div class="text-center">
-                                        <img src="images/photo.jpg" alt="About Me" class="rounded-circle">
-
-                                        <p>Quis vero phasellus hac nullam, in quam vitae duis adipiscing mauris leo, laoreet eget at quis, ante vestibulum vivamus vel. Sapien lobortis, eget orci purus amet pede, consectetur neque risus.</p>
-                                    </div>
-
-                                </aside>
-
-                                <aside class="widget about-widget">
-                                    <div class="widget-title">Subscribe & Follow</div>
-
-                                    <ul class="socials">
-                                        <li><a href="http://facebook.com"><i class="mdi mdi-facebook"></i></a></li>
-                                        <li><a href="http://twitter.com"><i class="mdi mdi-twitter"></i></a></li>
-                                        <li><a href="http://instagram.com"><i class="mdi mdi-instagram"></i></a></li>
-                                        <li><a href="http://pinterest.com"><i class="mdi mdi-pinterest"></i></a></li>
-                                    </ul>
-
-                                </aside>
-
-                                <!-- Categories widget-->
-                                <aside class="widget widget_categories">
-                                    <div class="widget-title">Categories</div>
-                                    <ul>
-                                        <li><a href="#">Journey</a> (40)</li>
-                                        <li><a href="#">Photography</a> (08)</li>
-                                        <li><a href="#">Lifestyle</a> (11)</li>
-                                        <li><a href="#">Food & Drinks</a> (21)</li>
-                                    </ul>
-                                </aside>
-
-                                <!-- Recent entries widget-->
-                                <aside class="widget widget_recent_entries_custom">
-                                    <div class="widget-title">Popular Posts</div>
-                                    <ul>
-                                        <li class="clearfix">
-                                            <div class="wi">
-                                                <a href="#"><img src="images/works/img1.jpg" alt="" class="img-fluid"></a>
-                                            </div>
-                                            <div class="wb"><a href="#">Beautiful Day With Friends..</a> <span class="post-date">Jun 15, 2017</span></div>
-                                        </li>
-                                        <li class="clearfix">
-                                            <div class="wi">
-                                                <a href="#"><img src="images/works/img2.jpg" alt="" class="img-fluid"></a>
-                                            </div>
-                                            <div class="wb"><a href="#">Nature valley with cooling..</a> <span class="post-date">Jun 10, 2017</span></div>
-                                        </li>
-                                        <li class="clearfix">
-                                            <div class="wi">
-                                                <a href="#"><img src="images/works/img3.jpg" alt="" class="img-fluid"></a>
-                                            </div>
-                                            <div class="wb"><a href="#">15 Best Healthy and Easy Salad..</a> <span class="post-date">Jun 8, 2017</span></div>
-                                        </li>
-                                    </ul>
-                                </aside>
-
-                                <!-- Text widget-->
-                                <aside class="widget">
-                                    <div class="widget-title">Text Widget</div>
-
-                                    <p class="text-muted text-widget-des">Exercitation photo booth stumptown tote bag Banksy, elit small batch freegan sed. Craft beer elit seitan exercitation, photo booth et 8-bit kale chips proident chillwave deep v laborum. Aliquip veniam delectus, Marfa eiusmod Pinterest in do umami readymade swag. </p>
-
-                                </aside>
-
-                                <!-- Archives widget-->
-                                <aside class="widget">
-                                    <div class="widget-title">Archives</div>
-
-                                    <ul>
-                                        <li><a href="#">March 2017</a> (40)</li>
-                                        <li><a href="#">April 2017</a> (08)</li>
-                                        <li><a href="#">May 2017</a> (11)</li>
-                                        <li><a href="#">Jun 2017</a> (21)</li>
-                                    </ul>
-
-                                </aside>
-
-                                <!-- Tags widget-->
-                                <aside class="widget widget_tag_cloud">
-                                    <div class="widget-title">Tags</div>
-                                    <div class="tagcloud">
-                                        <a href="#">logo</a>
-                                        <a href="#">business</a>
-                                        <a href="#">corporate</a>
-                                        <a href="#">e-commerce</a>
-                                        <a href="#">agency</a>
-                                        <a href="#">responsive</a>
-                                    </div>
-                                </aside>
-                            </div>
-                        </div>
-                        <!-- Sidebar end-->
                     </div>
 
                 </div>
@@ -290,10 +184,5 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home', components: { HelloWorld }
-}
 </script>
