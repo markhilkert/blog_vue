4140f77ec0d29c101e8506584fc5d5a35e3c3dbc
markhilkert <mark.hilkert@gmail.com>
2019-04-15 13:16:47 -0700

    generalized the post page & added dompurify

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 Vue.config.productionTip = false
 
 import axios from "axios";
+import DOMPurify from "dompurify";
 
 axios.defaults.baseURL = 
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
